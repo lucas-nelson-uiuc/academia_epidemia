71d925963d39d46a2db362c47c07ac4fa26f7d40
lln2 <lln2@illinois.edu>
2021-01-30 22:15:36 -0600

    becoming familiar with API

--- stravaFreemium/strava.py
+++ stravaFreemium/strava.py
@@ -1,1 +1,10 @@
-create strava.py
+import bs4
+import webbrowser
+import requests
+
+strava_URL = 'https://www.strava.com/dashboard?feed_type=my_activity'
+strava_request = requests.get(strava_URL)
+strava_bs4 = bs4.BeautifulSoup(strava_request.content, 'html.parser')
+
+strava_activites = strava_bs4.find_all('div')
+print(strava_activites)
