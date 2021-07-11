6f6490a7a516d9984f947c45a6dfad5999424758
Lucas Nelson <lucas.nelson.uiuc@gmail.com>
2021-07-10 21:17:41 -0500

    add multiple_playlists() function

--- spotify_freemium/spotify_agent.py
+++ spotify_freemium/spotify_agent.py
@@ -2,55 +2,99 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 import spotipy.oauth2 as oauth2
 import pandas as pd
-import time
+import numpy as np
+import re
+from datetime import datetime
+
+class Error(Exception):
+	"""Base class for all exceptions"""
+	pass
+
+class URLError(Error):
+	"""Invalid URL passed"""
+	pass
 
 class SpotifyUser():
 
-	def __init__(self):
-		self.client_id = ''
-		self.client_secret = ''
+	def __init__(self, client_id, client_secret):
+		self.client_id = client_id
+		self.client_secret = client_secret
 
-def get_playlist_df(spotify_user, url):
+
+def get_playlist_df(spotify_user, playlist_url):
+	'''Input: user token (client_id, client_secret), url of playlist
+	Output: df containing audio features/analysis of every track in playlist
+	'''
+
 
 	auth_manager = SpotifyClientCredentials(client_id = spotify_user.client_id,
-						client_secret = spotify_user.client_secret)
+											client_secret = spotify_user.client_secret)
 	sp = spotipy.Spotify(auth_manager=auth_manager)
 
 
-	# gather track details
-	details_labels = ['album', 'artist', 'title', 'url', 'length', 'explicit', 'popularity', 'number']
-	track_details = []
-	for item in sp.playlist_tracks(url)['items']:
-		track_album = item['track']['album']['name']
-		track_artist = item['track']['artists'][0]['name']
-		track_title = item['track']['name']
-		track_url = item['track']['external_urls']['spotify']
-		track_length = item['track']['duration_ms']
-		track_explicit = item['track']['explicit']
-		track_popularity = item['track']['popularity']
-		track_number = item['track']['track_number']
-		track_details.append([track_album, track_artist, track_title, track_url, track_length,
-					track_explicit, track_popularity, track_number])
-
-	# gather track features
+	details_labels = ['title', 'artist', 'album', 'genre', 'url', 'length', 'explicit', 'popularity',
+					'artist_date', 'user_date', 'user_time']
 	features_labels = ['danceability', 'energy', 'loudness', 'speechiness', 'acousticness', 'instrumentalness',
 						'liveness', 'valence', 'tempo', 'signature']
+	track_details = []
 	track_features = []
-	for track in track_details:
-	   audio_features = sp.audio_features(track[3])[0]
-	   track_danceability = audio_features['danceability']
-	   track_energy = audio_features['energy']
-	   track_loudness = audio_features['loudness']
-	   track_speechiness = audio_features['speechiness']
-	   track_acousticness = audio_features['acousticness']
-	   track_instrumentalness = audio_features['instrumentalness']
-	   track_liveness = audio_features['liveness']
-	   track_valence = audio_features['valence']
-	   track_tempo = audio_features['tempo']
-	   track_signature = audio_features['time_signature']
-	   track_features.append([track_danceability, track_energy, track_loudness, track_speechiness,
-                            track_acousticness, track_instrumentalness, track_liveness, track_valence,
-                            track_tempo, track_signature])
+	playlist_index = 0
+
+	while len(sp.playlist_tracks(playlist_url, offset = playlist_index)['items']) != 0:
+
+		# gather track details
+		for item in sp.playlist_tracks(playlist_url, offset = playlist_index)['items']:
+			track_title = item['track']['name']
+			track_artist = item['track']['artists'][0]['name']
+			track_album = item['track']['album']['name']
+			track_url = item['track']['external_urls']['spotify']
+			track_length = item['track']['duration_ms']
+			track_explicit = item['track']['explicit']
+			track_popularity = item['track']['popularity']
+
+			# add details for track genre(s)
+			artist_genres = sp.artist(item['track']['album']['artists'][0]['external_urls']['spotify'])['genres']
+			if len(artist_genres) == 0:
+				track_genre = 'NA'
+			else:
+				track_genre = artist_genres[0]
+
+			# add details for when track was added by artist
+			track_added = item['track']['album']['release_date']
+			try:
+				match = re.search('([0-9]{4}-[0-9]{2}-[0-9]{2})', track_added)
+				track_date = datetime.strptime(match.group(1), '%Y-%m-%d').date()
+			except:
+				proxy_date = track_added + '-01-01'
+				track_date = datetime.strptime(proxy_date, '%Y-%m-%d').date()
+			
+			# add details for when track was added by SpotifyUser
+			added_at = item['added_at']
+			match = re.search('([0-9]{4}-[0-9]{2}-[0-9]{2})T([0-9]{2}:[0-9]{2}:[0-9]{2})Z', added_at)
+			added_date = datetime.strptime(match.group(1), '%Y-%m-%d').date()
+			added_time = datetime.strptime(match.group(2), '%H:%M:%S').time()
+
+			track_details.append([track_title, track_artist, track_album, track_genre, track_url, track_length,
+									track_explicit, track_popularity, track_date, added_date, added_time])
+		
+		# gather track features
+		for track in track_details:
+			audio_features = sp.audio_features(track[4])[0]
+			track_danceability = audio_features['danceability']
+			track_energy = audio_features['energy']
+			track_loudness = audio_features['loudness']
+			track_speechiness = audio_features['speechiness']
+			track_acousticness = audio_features['acousticness']
+			track_instrumentalness = audio_features['instrumentalness']
+			track_liveness = audio_features['liveness']
+			track_valence = audio_features['valence']
+			track_tempo = audio_features['tempo']
+			track_signature = audio_features['time_signature']
+			track_features.append([track_danceability, track_energy, track_loudness, track_speechiness,
+								track_acousticness, track_instrumentalness, track_liveness, track_valence,
+								track_tempo, track_signature])
+
+		playlist_index += 100
 
 	# combine details and features per track
 	for track, features in zip(track_details, track_features):
@@ -58,12 +102,43 @@ def get_playlist_df(spotify_user, url):
 
 	# combine labels
 	details_labels.extend(features_labels)
-
 	playlist_df = pd.DataFrame(track_details, columns=details_labels)
-	print(playlist_df.head(60))
+	
+	# add playlist name
+	# playlist_df['playlist'] = sp.playlist(playlist_url)['name']
+	
+	return playlist_df
+
+
+def multiple_playlists(spotify_user, playlist_url_string):
+	
+	auth_manager = SpotifyClientCredentials(client_id = spotify_user.client_id,
+											client_secret = spotify_user.client_secret)
+	sp = spotipy.Spotify(auth_manager=auth_manager)
+
+	playlist_df_list = []
+	# playlist_names = []
+	
+	for playlist_url in playlist_url_string.split(','):
+		
+		try:
+			playlist_url = playlist_url.strip()
+			playlist_df_list.append(get_playlist_df(spotify_user, playlist_url))
+			# playlist_names.append()
+
+		except:
+			print('Sorry. Could not obtain all playlists. Please ensure URLs entered correctly.')
+
+	return pd.concat(playlist_df_list).drop_duplicates()
+	
 
 if __name__ == '__main__':
-	user = SpotifyUser()
-	user.client_id = # enter client_id
-	user.client_secret = # enter client_secret
-	get_playlist_df(user, 'URL/URI/id')
+	user = SpotifyUser(client_id='3369a177760443e1ba8fdc24ffe8ee3a',
+						client_secret='26557253d83447879b0ff7251d291517')
+
+	playlists_str = """https://open.spotify.com/playlist/7fzOIIrfWEifFp64mZr0Fp,
+						https://open.spotify.com/playlist/4ruz6qz9UaJi0Uh9aXWd4e ,
+						https://open.spotify.com/playlist/3PFpKt44V2PP5IvNqCn1ly
+	"""
+
+	multiple_playlists(user, playlists_str)
